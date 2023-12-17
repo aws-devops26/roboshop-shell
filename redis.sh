cb23d4f25756c87f8aa37a15c506386377df81b5
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-17 18:58:26 +0530

    roboshop shell script

--- redis.sh
+++ redis.sh
@@ -30,7 +30,7 @@ dnf module enable redis:remi-6.2 -y &>>$LOGFILE
 VALIDATE $? " enabiling redis"
 dnf install redis -y &>>$LOGFILE
 VALIDATE $? " installing erdis"
-sed -i 's/127.0.0.1/0.0.0.0/g ' /etc/redis.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g '  /etc/redis/redis.conf &>>$LOGFILE
 VALIDATE $? "allowing remote connections"
 systemctl enable redis &>>$LOGFILE
 VALIDATE $? "Enabiling redis"
