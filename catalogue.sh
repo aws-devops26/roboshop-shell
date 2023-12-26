7a806836fcc9ea4eb912af99667b511602f4892b
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-26 23:05:11 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -26,9 +26,9 @@ if [ $id -ne 0 ]
 fi
 dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? " disabiling current node js"
-dnf module enable nodejs:18 -y &>>$LOGFILE
+dnf module enable nodejs:18 -y &>> $LOGFILE
  VALIDATE $? " enabiling current node js"
- dnf install nodejs -y &>>$LOGFILE
+ dnf install nodejs -y &>> $LOGFILE
  VALIDATE $? " installing node js"
  id roboshop
 if [ $? -ne 0 ]
@@ -43,20 +43,20 @@ fi
  curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
  VALIDATE $? "downloading catalogue application"
  cd /app 
-unzip -o /tmp/catalogue.zip &>>$LOGFILE
+unzip -o /tmp/catalogue.zip &>> $LOGFILE
 VALIDATE $? "unziping catalogue"
-npm install &>>$LOGFILE
+npm install &>> $LOGFILE
 VALIDATE $? "installing dependencies"
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 VALIDATE $? "copying catalogue service file"
-systemctl daemon-reload &>>$LOGFILE
+systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? " reloading demon"
-systemctl enable catalogue &>>$LOGFILE
+systemctl enable catalogue &>> $LOGFILE
 VALIDATE $? "Enabiling catalogue"
-systemctl start catalogue &>>$LOGFILE
+systemctl start catalogue &>> $LOGFILE
 VALIDATE $? "starting catalogue"  
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? "copying mongodb service file"
-dnf install mongodb-org-shell -y &>>$LOGFILE
+dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "installing mongodb client"
-mongo --host mongodb.awssrivalli.online </app/schema/catalogue.js &>>$LOGFILE
+mongo --host mongodb.awssrivalli.online </app/schema/catalogue.js &>> $LOGFILE
