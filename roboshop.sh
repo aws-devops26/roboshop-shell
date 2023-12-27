631f0e0032f28006a81b7108d241210152b2811a
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-27 23:02:39 +0530

    roboshop shell with ansible

--- roboshop.sh
+++ roboshop.sh
@@ -14,6 +14,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-070a9eb43e6d6e9eb
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-sg-070a9eb43e6d6e9eb
 done
    
