d248e3a8331bb7a437af1b93fbd853bfca993995
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-27 23:30:52 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-070a9eb43e6d6e9eb #replace with your SG ID 
+SG_ID=sg-070a9eb43e6d6e9eb #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=ZZ025434420MOIOXIRVMIF # replace your zone ID
+DOMAIN_NAME="awssrivalli.online"
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instances is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -14,6 +15,26 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-sg-070a9eb43e6d6e9eb
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-070a9eb43e6d6e9eb --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
-   
