83dba1cd3270647d3ff333597ab34030ceffe6db
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-29 18:41:45 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb #ami-03265a0778a880afb
-SG_ID=sg-070a9eb43e6d6e9eb #replace with your SG ID sg-070a9eb43e6d6e9eb
+SG_ID=sg-05aeae68f59325217 #replace with your SG ID sg-05aeae68f59325217
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 for i in "${INSTANCES[@]}"
@@ -13,6 +13,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-070a9eb43e6d6e9eb 
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-05aeae68f59325217
 
 done
