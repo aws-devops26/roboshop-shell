acdfbcce7b08435bfbf4613651210b159bf8f620
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-29 19:02:49 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,6 +15,6 @@ do
 
     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type 
     $INSTANCE_TYPE --security-group-ids sg-05aeae68f59325217 --tag-specifications 
-    "ResourceType=instance,Tags=[{key=name,value=$i}]"
+    "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
 done
