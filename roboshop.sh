bc51683f657315fc5c4f5cb5416f4001ed2c05d2
aws-devops26 <srivalli26.najara@gmail.com>
2024-12-16 21:53:39 +0530

    main

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AMI=ami-0b4f379183e5706b9  #ami-0b4f379183e5706b9
-SG_ID=sg-0e94719a08dddaf21  #replace with your SG ID sg-0541e7f1344e449e2
+SG_ID=sg-0541e7f1344e449e2 #replace with your SG ID sg-0541e7f1344e449e2
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z09268031N95BFUT9OPZ7  #route 53 srivalli.shop zone id
 DOMAIN_NAME="srivalli.shop"
@@ -14,7 +14,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0e94719a08dddaf21 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0541e7f1344e449e2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i : $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
